a4509fd2e511353f32644e21b00dbf939febc25a
William Wong <william.wai.lim.wong@gmail.com>
2023-08-13 13:51:20 -0400

    passes tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    filepath = f'{file_name}.txt'
+    with open(filepath, mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    filepath = f'{file_name}.txt'
+    with open(filepath, mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    filepath = f'{file_name}.txt'
+    with open(filepath, encoding='utf-8') as log_file:
+        return log_file.read()
+
